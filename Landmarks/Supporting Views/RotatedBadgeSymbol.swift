74c77ce0de68fec8f8962f069b497145400ff6e3
Howard Edwards <howarde.edwards@gmail.com>
2019-10-06 06:34:25 -0500

    complete

---
+++ Landmarks/Supporting Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Howard Edwards on 10/6/19.
+//  Copyright © 2019 Howard Edwards. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
